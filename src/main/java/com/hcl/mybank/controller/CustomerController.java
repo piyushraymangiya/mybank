3bf644d02e772b51b8cc7c0e2af96e1322c7fbb1
monikanidhisingh <monikanidhisingh@gmail.com>
2019-08-03 12:54:08 +0530

    customer

--- src/main/java/com/hcl/mybank/controller/CustomerController.java
+++ src/main/java/com/hcl/mybank/controller/CustomerController.java
@@ -1,8 +1,25 @@
 package com.hcl.mybank.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.hcl.mybank.dto.CustomerDto;
+
+@CrossOrigin("*")
+@RestController("/customers")
 public class CustomerController {
+	
+	@PostMapping("/login")
+	public ResponseEntity<Object> getAccountSummery(@RequestBody CustomerDto customerdto){
+		
+		
+		return null;
+		
+	}
+	
+	
 
 }
