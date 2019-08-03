afc2c4fcf4146a637a3b3d11241681dfd4905b3e
piyush <piyushr999@gmail.com>
2019-08-03 13:43:55 +0530

    Resolved running errors

--- src/main/java/com/hcl/mybank/service/CustomerService.java
+++ src/main/java/com/hcl/mybank/service/CustomerService.java
@@ -1,11 +1,8 @@
 package com.hcl.mybank.service;
 
-import org.springframework.stereotype.Component;
-
 import com.hcl.mybank.dto.CustomerDto;
 import com.hcl.mybank.dto.ResponseDto;
 
-@Component
 public interface CustomerService {
 
 	public ResponseDto getCustomerAccountSummery(CustomerDto customerDto);
