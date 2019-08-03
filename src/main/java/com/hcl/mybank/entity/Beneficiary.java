fe2de35fbe4f41c16a277c1876be5b06aa1babdd
piyush <piyushr999@gmail.com>
2019-08-03 14:13:29 +0530

    Benificery Mapping

--- src/main/java/com/hcl/mybank/entity/Beneficiary.java
+++ src/main/java/com/hcl/mybank/entity/Beneficiary.java
@@ -4,24 +4,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import lombok.Data;
 
-
 @Data
 @Entity
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-public class Beneficiary {	
+public class Beneficiary {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	private Long customerId;
-	
-	private Long beneficiaryId;
 
+	@ManyToOne
+	@JoinColumn(name = "customerId")
+	private Customer customerId;
+
+	@ManyToOne
+	@JoinColumn(name = "beneficiaryId")
+	private Customer beneficiaryId;
 
 }
