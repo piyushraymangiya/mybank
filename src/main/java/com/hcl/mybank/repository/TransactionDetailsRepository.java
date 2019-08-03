b03d9aadd5b907902a593948a9eac41612cbe4a8
Rohan Beldare <rohanbeldare@gmail.com>
2019-08-03 13:24:39 +0530

    added repos

---
+++ src/main/java/com/hcl/mybank/repository/TransactionDetailsRepository.java
@@ -0,0 +1,16 @@
+package com.hcl.mybank.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hcl.mybank.entity.TransactionDetail;
+
+@Repository
+public interface TransactionDetailsRepository extends JpaRepository<TransactionDetail, Long>{
+
+	List<TransactionDetail> findByToAccountNumberEquals(String accountNumber);
+
+	
+}
